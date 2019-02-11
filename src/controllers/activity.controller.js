9abccffa89c32525826989e667324135cfd707cc
amanshawar <amanshawar_a@hotmail.com>
2019-02-11 14:46:18 +0200

    add index function #6

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -1,12 +1,14 @@
+const Program = require('../models/Program'); // eslint-disable-line no-unused-vars
+const Objective = require('../models/Objective'); // eslint-disable-line no-unused-vars
 const Activity = require('../models/Activity');
 
 const add = (req, res) => {
   const {
-    name, hours, startDate, endDate, trainerName, location, ProgramId, ObjectiveId,
+    name, hours, startDate, endDate, trainerName, location, program, objective,
   } = req.body;
 
   const activity = new Activity({
-    name, hours, startDate, endDate, trainerName, location, ProgramId, ObjectiveId,
+    name, hours, startDate, endDate, trainerName, location, program, objective,
   });
   activity.save()
     .then(() => {
@@ -16,5 +18,16 @@ const add = (req, res) => {
     });
 };
 
-
-module.exports = { add };
+const index = (req, res) => {
+  Activity.find({})
+    .populate('program')
+    .populate('objective')
+    .exec((err, activities) => {
+      if (err) {
+        res.json({ success: false, error: err.message });
+      } else {
+        res.json({ success: true, data: activities });
+      }
+    });
+};
+module.exports = { add, index };
