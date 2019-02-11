48459c7c42b292ccc2bdd1fbba86bf7effd3301e
amanshawar <amanshawar_a@hotmail.com>
2019-02-11 09:32:26 +0200

    solve comments

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello world');
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
+
 module.exports = router;
