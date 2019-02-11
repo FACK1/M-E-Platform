0ee1e5e5c82a0e9204b6c92d7a6bdf5f9127d2a1
amanshawar <amanshawar_a@hotmail.com>
2019-02-11 11:33:49 +0200

    eslint fixing

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -1,15 +1,19 @@
 const Activity = require('../models/Activity.model');
 
-const store = (req,res) => {
-const { name ,hours ,startDate ,endDate ,trainerName ,location,ProgramId,ObjectiveId} = req.body;
+const store = (req, res) => {
+  const {
+    name, hours, startDate, endDate, trainerName, location, ProgramId, ObjectiveId,
+  } = req.body;
 
-const activity= new Activity({name ,hours,startDate,endDate,trainerName,location,ProgramId,ObjectiveId});
-activity.save()
-.then(() => {
-res.send({state:true});
-}).catch((err) => {
-  res.send({state:false,error:err.message});
-});
-}
+  const activity = new Activity({
+    name, hours, startDate, endDate, trainerName, location, ProgramId, ObjectiveId,
+  });
+  activity.save()
+    .then(() => {
+      res.send({ state: true });
+    }).catch((err) => {
+      res.send({ state: false, error: err.message });
+    });
+};
 
-module.exports = {store};
+module.exports = { store };
