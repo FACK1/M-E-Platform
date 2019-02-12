0c83cd093898959a6a7b501480d076ffb96800ce
amanshawar <amanshawar_a@hotmail.com>
2019-02-12 11:24:49 +0200

    update the style of the header #32

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import "react-datepicker/dist/react-datepicker.css";
-import {StyledHeader, StyledUl, StyledLi,StyledLink} from './index.style'
+import {StyledHeader, StyledUl, StyledLi,StyledLink,StyledBgLogo,StyledLogo,StyledButton} from './index.style'
+import bgLogo from '../../assets/background-YDRC-Logo.png';
+import logo from '../../assets/YDRC-Logo.png';
 
 
 const Header = () =>{
     return (
       <StyledHeader>
+        <StyledLogo src={logo} />
         <StyledUl className="header">
-          <StyledLi><StyledLink to="/ViewActivities">الأنشطة</StyledLink ></StyledLi>
-          <StyledLi><StyledLink  to="/Programs">البرامج</StyledLink ></StyledLi>
-          <StyledLi><StyledLink  to="/contact">المستخدمين</StyledLink ></StyledLi>
-          <StyledLi><StyledLink  to="/objectives">الأنشطة</StyledLink ></StyledLi>
-          <StyledLi><StyledLink  to="/Programs">البرامج</StyledLink ></StyledLi>
-          <StyledLi><StyledLink  to="/contact">المستخدمين</StyledLink ></StyledLi>
+            <StyledLi><StyledLink to="/ViewActivities">الأنشطة</StyledLink ></StyledLi>
+            <StyledLi><StyledLink  to="/Programs">البرامج</StyledLink ></StyledLi>
+            <StyledLi><StyledLink  to="/contact">المستخدمين</StyledLink ></StyledLi>
+            <StyledLi><StyledLink  to="/objectives">التقارير</StyledLink ></StyledLi>
+            <StyledLi><StyledLink  to="/Programs">الأهداف</StyledLink ></StyledLi>
+            <StyledLi><StyledButton>تسجيل الخروج</StyledButton></StyledLi>
         </StyledUl>
+        <StyledBgLogo src={bgLogo}/>
       </StyledHeader>
     );
   }
--- client/src/components/Header/index.style.js
+++ client/src/components/Header/index.style.js
@@ -1,27 +1,52 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-
 export const StyledHeader = styled.div`
+display: flex;
+flex-direction:column;
 width:20%;
+z-index: -1;
+top: 0;
+right: 0;
+position:fixed;
 float:right;
-background-color: #f1f1f1;
+background-image: linear-gradient(-180deg, rgba(25,179,212,1), rgba(255,255,255,0));
+background-color:  #4BC0D8;
 height:100vh;
-margin: 0;
 padding: 0;
 `;
+export const StyledBgLogo= styled.img`
+
+`;
+export const StyledLogo= styled.img`
+width:80%;
+align-self:center;
+padding: 0px 22px;
+`;
 export const StyledUl = styled.ul`
+margin-top:-50px;
 list-style-type: none;
 width:100%;
-margin: 0;
-padding: 0;
-background-color: #f1f1f1;
+padding: 20%;
 `;
 export const StyledLink = styled(Link)`
 display: block;
-color: #000;
-padding: 8px 16px;
+color: white;
+font-family: 'Cairo', sans-serif;
+font-size:2vw;
+padding: 15px 22px;
 text-decoration: none;
 `;
 export const StyledLi = styled.li`
+width:100%;
+`;
+export const StyledButton = styled.button`
+background-color:#308092;
+font-family: 'Cairo', sans-serif;
+color: white;
+padding: 0px 22px;
 
+text-align: center;
+font-size: 1.5vw;
+border-radius: 20.5px;
+border:0px;
 `;
