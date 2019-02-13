73f50f084a9d01c7b29bd53f87aca685c1e2ba53
amanshawar <amanshawar_a@hotmail.com>
2019-02-13 14:01:08 +0200

    make th ebutton component  #23

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+import {StyledButton} from "./index.style";
+
+class Button extends Component {
+    render () {
+      return(
+        <StyledButton>
+          انشاء نشاط جديد
+        </StyledButton>
+      )
+    }
+}
+
+export default Button;
---
+++ client/src/components/Button/index.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+border: 1px solid #ccc;
+width: 200px;
+height: 30px;
+border-radius: 50px;
+background-color:#08A3C3;
+text-align: center;
+color:white;
+margin-top:15px;
+margin-left:500px;
+`;
