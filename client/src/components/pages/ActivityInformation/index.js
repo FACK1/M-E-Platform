b01829bfedd9d61b34dbd8affb64495e4700911b
razantayyem <razanr543@gmail.com>
2019-02-14 11:25:20 +0200

    Create initial ActivityInformation page  #2

--- client/src/components/pages/ActivityInformation/index.js
+++ client/src/components/pages/ActivityInformation/index.js
@@ -1,1 +1,22 @@
 
+import React, { Component } from 'react';
+import {Title, StyledPage} from "./index.style";
+import Header from "../../Header";
+
+
+
+ class ActivityInformation extends Component {
+  render() {
+    return (
+        <React.Fragment>
+        <Header/>
+        <StyledPage>
+           <Title> تفاصيل النشاط </Title>
+        </StyledPage>
+        </React.Fragment>
+
+     );
+    }
+  }
+
+ export default ActivityInformation;
