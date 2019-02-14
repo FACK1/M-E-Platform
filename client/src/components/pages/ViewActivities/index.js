8e7b12a02e399307d69a07492c9bfd439d529806
amanshawar <amanshawar_a@hotmail.com>
2019-02-14 11:55:27 +0200

    connect table to view page #6

--- client/src/components/pages/ViewActivities/index.js
+++ client/src/components/pages/ViewActivities/index.js
@@ -1,18 +1,49 @@
+import axios from 'axios';
 import React, { Component } from "react";
-import "./index.css";
 import {Link} from 'react-router-dom'
 import Header from "../../Header";
-
+import Table from "../../Table";
+import {StyledPage, StyledTilte} from "./index.style";
 
 
 class ViewActivities extends Component {
+
+
+  getColumnsList(){
+
+    const columns=[
+    { Header: 'اسم النشاط'},
+    { Header:'اسم المدرب'},
+    { Header: 'عدد الساعات'},
+    { Header: 'تاريخ البداية'},
+    { Header: 'تاريخ النهاية'},
+    { Header: 'الموقع'},
+    { Header:'البرنامج'},
+    { Header: 'الهدف'}]
+    return columns;
+  }
+
+
+  componentDidMount() {
+   axios
+     .get("/activities")
+     .then(data => {
+       const result = data.data;
+       console.log("result",result.data);
+       this.setState({result:result.data});
+     }).catch(error => {
+       console.log(error);
+     });
+ }
   render() {
     return (
       <React.Fragment>
-        <h1 className='title'> جميع الانشطة </h1>
         <Header/>
-        <li><Link to="/CreateActivity">اضافة نشاط جديد</Link></li>
-
+        <StyledPage>
+          <StyledTilte> جميع الانشطة </StyledTilte>
+          <Table columns={this.getColumnsList()} data={this.state.result}/>
+          <li><Link to="/CreateActivity">اضافة نشاط جديد</Link></li>
+        </StyledPage>
       </React.Fragment>
     );
   }
