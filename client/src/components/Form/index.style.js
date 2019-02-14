012f49c920426a329b811b5f662a0e9048df6177
mhmdtshref <mhmdtshref@gmail.com>
2019-02-14 08:34:25 +0200

    Remove unneeded comments and unused exported variables #35 #37

--- client/src/components/Form/index.style.js
+++ client/src/components/Form/index.style.js
@@ -3,15 +3,6 @@ import { Form, Field } from "formik";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 
-export const StyledContainerBtn=styled.div`
-margin-right:422px;
-`;
-export const Title = styled.h1`
-  font-family: "Raleway", sans-serif;
-  font-weight: 600;
-  color: #4d4d4d;
-  font-size: 2.2em;
-`;
 export const StyledForm = styled(Form)`
   display: flex;
   justify-content: space-between;
@@ -55,36 +46,3 @@ export const StyledDatePicker = styled(DatePicker)`
     padding-right: 27px;
   }
 `;
-
-export const StyledButton = styled.button`
-border: 1px solid #ccc;
-width: 200px;
-height: 30px;
-border-radius: 50px;
-background-color:#08A3C3;
-text-align: center;
-color:white;
-margin-top:15px;
-margin-left:500px;
-`;
-/*export const StyledDatePicker = styled(DatePicker)`
-=======
-export const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const StyledDatePicker = styled(DatePicker)`
->>>>>>> master
-  height: 35px;
-  border: 1px solid #ccc;
-  background-color: #fff;
-  width: 200px;
-  height: 30px;
-  border-radius: 50px;
-  border-color:#08A3C3;
-
-  ::placeholder {
-    padding-right: 27px;
-  }
-`;*/
