c70f1057360d938002a2880770012a53b9323b9c
razantayyem <razanr543@gmail.com>
2019-02-14 14:48:36 +0200

    Modify the activity details nav component style #2

--- client/src/components/ActivityDetails/index.style.js
+++ client/src/components/ActivityDetails/index.style.js
@@ -8,11 +8,20 @@ margin-right: 20%;
 export const StyledLiTitle = styled.h1`
   font-weight: 600;
   color: #4d4d4d;
-  font-size: 2.2em;
+  font-size: 1.2em;
+  margin-bottom:20px;
+
 `;
 
 export const StyledUl = styled.ul`
+margin: 40px 30% 0px 0px;
+padding:0px;
+list-style-type: none;
+width:70%;
 `;
 
 export const StyledDetail = styled.li`
+width:100%;
+margin-bottom:20px;
+
 `;
