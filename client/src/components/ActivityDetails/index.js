569e1a5a192920a5f31004a56b3090558fef9ae6
mhmdtshref <mhmdtshref@gmail.com>
2019-02-15 14:59:02 +0200

    Use axios request instead of mock data #2

--- client/src/components/ActivityDetails/index.js
+++ client/src/components/ActivityDetails/index.js
@@ -1,42 +1,68 @@
 import React, { Component } from "react";
 import {StyledLiTitle, StyledDetailLi, StyledUl} from "./index.style";
+import axios from 'axios';
 
 class ActivityDetails extends Component {
 
   constructor(props){
     super(props);
-    this.getTrainingDetails = this.getTrainingDetails.bind(this);
+    this.state = {
+      renderedDetails: <div>Loading...</div>,
+    };
+    this.getActivityDetailsById = this.getActivityDetailsById.bind(this);
   }
 
-getTrainingDetails() {
-  const details = this.props.details;
-  const renderedDetails = Object.keys(details).map((k) => {
+  componentDidMount(){
+    this.getActivityDetailsById();
+  }
+
+
+  getActivityDetailsById(){
+    const { activityId } = this.props;
+      axios.get('/activities/'+activityId)
+        .then(({ data }) => {
+          console.log("RESPONSE:", data);
+          if(data.success){
+            this.setState({ renderedDetails:  this.getTrainingDetails(data.data)});
+          } else {
+            alert("Error: "+data.err);
+          }
+        }).catch((err) => {
+          alert("Error: " + err.message)
+      });
+  }
+
+
+
+getTrainingDetails(detailsList) {
+  const renderedDetails = Object.keys(detailsList).map((k) => {
     switch (k) {
       case 'name':
-        return <StyledLiTitle>{details[k]}</StyledLiTitle>;
+        return <StyledLiTitle>{detailsList[k]}</StyledLiTitle>;
       case 'trainerName':
-        return <StyledDetailLi>{"المدرب:"} {details[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"المدرب:"} {detailsList[k]}</StyledDetailLi>;
       case 'place':
-        return <StyledDetailLi>{"الموقع:"} {details[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"الموقع:"} {detailsList[k]}</StyledDetailLi>;
       case 'hours':
-        return <StyledDetailLi>{"عدد الساعات:"} {details[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"عدد الساعات:"} {detailsList[k]}</StyledDetailLi>;
       case 'startDate':
-        return <StyledDetailLi>{"من تاريخ:"} {details[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"من تاريخ:"} {detailsList[k]}</StyledDetailLi>;
       case 'endDate':
-        return <StyledDetailLi>{"حتى تاريخ:"} {details[k]}</StyledDetailLi>;
-      default:
-        return <StyledDetailLi>{k}: {details[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"حتى تاريخ:"} {detailsList[k]}</StyledDetailLi>;
+      // default:
+      //   return <StyledDetailLi>{k}: {details[k]}</StyledDetailLi>;
     }
   });
   return renderedDetails;
 }
 
 
+
   render() {
     return (
       <React.Fragment>
       <StyledUl>
-        {this.getTrainingDetails()}
+        {this.state.renderedDetails}
         </StyledUl>
       </React.Fragment>
     );
