27802dca41c15ab9e535080f45422593e1e9b848
razantayyem <razanr543@gmail.com>
2019-02-17 15:41:18 +0200

    Create initial ViewPrograms page #4 #55

---
+++ client/src/components/pages/ViewPrograms/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Header from "../../Header";
+import { StyledPage, StyledTilte } from "./index.style";
+
+class ViewPrograms extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <StyledPage>
+          <StyledTilte> جميع البرامج </StyledTilte>
+        </StyledPage>
+      </React.Fragment>
+    );
+  }
+}
+export default ViewPrograms;
