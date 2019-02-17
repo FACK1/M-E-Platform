5f4fc6bfa2d776d8be1e370ee3c851f30841c6fc
razantayyem <razanr543@gmail.com>
2019-02-17 13:13:25 +0200

    Add program Form from MainForm Components #42

--- client/src/components/pages/CreateProgram/index.js
+++ client/src/components/pages/CreateProgram/index.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
-import {Title,StyledPage} from "./index.style";
+import React, { Component } from "react";
+import axios from 'axios';
 import Header from "../../Header";
+import MainForm from '../../Form'
+import {Title, StyledPage} from "./index.style";
 
- class CreateProgram extends Component {
+class CreateProgram extends Component {
 
-  constructor(props) {
-    super(props);
+  getFieldList() {
+    const fields = [
+      {
+        label: "اسم البرنامج",
+        type: "text",
+        name: "programName",
+        placeholder: "اسم البرنامج",
+      },{
+        label: "اسم المؤسسة",
+        type: "text",
+        name: "companyName",
+        placeholder: "اسم المؤسسة",
+      }
+    ];
+    return fields;
+  }
+
+  submitAction(values) {
+    axios.post('/CreateProgram', values);
   }
 
   render() {
     return (
-        <React.Fragment>
+      <React.Fragment>
         <Header/>
         <StyledPage>
-          <Title>  انشئ برنامجا جديداً </Title>
+          <Title> انشئ برنامجا جديداً </Title>
+          <MainForm fields={this.getFieldList()} action={this.submitAction} operationName="  انشاء برنامجا جديداً" />
         </StyledPage>
         </React.Fragment>
-
-     );
-    }
+    );
   }
-
- export default CreateProgram;
+}
+export default CreateProgram;
