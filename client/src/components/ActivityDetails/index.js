a63013840e34deca25491e457218f08070a9b44b
mhmdtshref <mhmdtshref@gmail.com>
2019-02-17 10:19:31 +0200

    Modify coding style #2

--- client/src/components/ActivityDetails/index.js
+++ client/src/components/ActivityDetails/index.js
@@ -20,48 +20,43 @@ class ActivityDetails extends Component {
 
   getActivityDetailsById(){
     const { activityId } = this.props;
-      axios.get('/activities/'+activityId)
+      axios.get(`/activities/${activityId}`)
         .then(({ data }) => {
           if(data.success){
             this.setState({ renderedDetails:  this.getTrainingDetails(data.data)});
           } else {
-            alert("Error: "+data.err);
+            alert(`Error: ${data.err}`);
           }
         }).catch((err) => {
-          alert("Error: " + err.message)
+          alert(`Error: ${err.message}`)
       });
   }
 
   dateParser(date){
-    //console.log(new Date(date).toLocaleString());
     const isoDate = ((new Date(date).toLocaleString()).split(',')[0]).split('/');
     return (isoDate[2] + '/' + isoDate[0] + '/' + isoDate[1]);
   }
 
 
 getTrainingDetails(detailsList) {
-  const renderedDetails = Object.keys(detailsList).map((k) => {
-    switch (k) {
+  const renderedDetails = Object.keys(detailsList).map((key) => {
+    switch (key) {
       case 'name':
-        return <StyledLiTitle>{detailsList[k]}</StyledLiTitle>;
+        return <StyledLiTitle>{detailsList[key]}</StyledLiTitle>;
       case 'trainerName':
-        return <StyledDetailLi>{"المدرب:"} {detailsList[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"المدرب:"} {detailsList[key]}</StyledDetailLi>;
       case 'place':
-        return <StyledDetailLi>{"الموقع:"} {detailsList[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"الموقع:"} {detailsList[key]}</StyledDetailLi>;
       case 'hours':
-        return <StyledDetailLi>{"عدد الساعات:"} {detailsList[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"عدد الساعات:"} {detailsList[key]}</StyledDetailLi>;
       case 'startDate':
-        return <StyledDetailLi>{"من تاريخ:"} {this.dateParser(detailsList[k])}</StyledDetailLi>;
+        return <StyledDetailLi>{"من تاريخ:"} {this.dateParser(detailsList[key])}</StyledDetailLi>;
       case 'endDate':
-        return <StyledDetailLi>{"حتى تاريخ:"} {this.dateParser(detailsList[k])}</StyledDetailLi>;
-      // default:
-      //   return <StyledDetailLi>{k}: {details[k]}</StyledDetailLi>;
+        return <StyledDetailLi>{"حتى تاريخ:"} {this.dateParser(detailsList[key])}</StyledDetailLi>;
     }
   });
   return renderedDetails;
 }
-
-
 
   render() {
     return (
--- client/src/components/ActivityDetails/index.style.js
+++ client/src/components/ActivityDetails/index.style.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 
 export const StyledPage = styled.div`
-margin-right: 20%;
+  margin-right: 20%;
 `;
 
 export const StyledUl = styled.ul`
-margin: 40px 15% 0px 0px;
-padding:0px;
-list-style-type: none;
-width:80%;
+  margin: 40px 15% 0px 0px;
+  padding:0px;
+  list-style-type: none;
+  width:80%;
 `;
 
 export const StyledLiTitle = styled.li`
@@ -20,7 +20,7 @@ export const StyledLiTitle = styled.li`
 `;
 
 export const StyledDetailLi = styled.li`
-width:100%;
-font-size: 1.5vw;
-margin-bottom:5vh;
+  width:100%;
+  font-size: 1.5vw;
+  margin-bottom:5vh;
 `;
