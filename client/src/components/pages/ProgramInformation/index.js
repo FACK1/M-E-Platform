b34450211043c7d3598472a42e198122f88afd3f
razantayyem <razanr543@gmail.com>
2019-02-18 15:20:57 +0200

    Adding ProgramDetails and Table component #63

--- client/src/components/pages/ProgramInformation/index.js
+++ client/src/components/pages/ProgramInformation/index.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import {Title, StyledPage} from "./index.style";
+import React, { Component } from "react";
+import { StyledPage, StyledProgramDetailsNav } from "./index.style";
 import Header from "../../Header";
+import ProgramDetails from "../../ProgramDetails";
+import Table from "../../Table";
 
-
-
- class ProgramInformation extends Component {
-  render() {
-    return (
-        <React.Fragment>
-        <Header/>
-        <StyledPage>
-           <Title> تفاصيل البرنامج </Title>
-        </StyledPage>
-        </React.Fragment>
-
-     );
-    }
+class ProgramInformation extends Component {
+  constructor(props) {
+    super(props);
   }
 
- export default ProgramInformation;
+  render() {
+    const { id } = this.props.match.params;
+    const columns = [
+      { Header: "id", accessor: "id", show: false },
+      { Header: "اسم النشاط", accessor: "name" },
+      { Header: "عدد الطلاب", accessor: "numberOfstudents" }
+    ];
+    return (
+      <React.Fragment>
+        <Header />
+        <StyledProgramDetailsNav>
+          <ProgramDetails programId={id} />
+        </StyledProgramDetailsNav>
+        <StyledPage>
+          <Table columns={columns} data={[]} />
+        </StyledPage>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ProgramInformation;
