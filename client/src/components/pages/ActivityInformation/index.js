7a788f060b3936c3c16849ec67d111d79e5becfb
mhmdtshref <mhmdtshref@gmail.com>
2019-02-18 16:00:14 +0200

    User suggesstions working #58

--- client/src/components/pages/ActivityInformation/index.js
+++ client/src/components/pages/ActivityInformation/index.js
@@ -3,58 +3,96 @@ import {StyledPage, StyledActivityDetailsNav} from "./index.style";
 import Header from "../../Header";
 import ActivityDetails from "../../ActivityDetails"
 import Table from '../../Table';
-import MainForm from '../../Form';
+import { Formik, Field } from 'formik';
 import axios from 'axios';
+import Button from '../../Button';
 
  class ActivityInformation extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+   constructor(props) {
+     super(props);
+     this.state = {
+       suggestions: [],
+     };
+     this.renderSuggestions = this.renderSuggestions.bind(this);
+     this.onChangeAction = this.onChangeAction.bind(this);
+     //this.onSubmitAddStudent = this.setUser.bind(this);
+   }
 
-  submitAction({ userName }) {
-    axios.get('/users/searchByName/'+ userName)
-      .then((response) => {
-        if(response.data.success) {
-          console.log(response.data.data);
-        } else {
-          console.log(response.data.err);
-        }
-      }).catch((err) => {
-        console.log(err.message);
-    });
-  }
+   onChangeAction({ target: { value } }) {
+     if(value){
+       axios.get('/users/searchByName/' + value)
+       .then(({ data }) => {
+         if (data.success) {
+           this.setState({ suggestions: data.data });
+         } else {
+           alert(`Getting Data Error: ${data.err}`);
+         }
+       })
+       .catch((err) => {
+         alert(`Getting Data Error: ${err.message}`);
+       });
+     }
+     console.log("State:", this.state.suggestions);
+   }
 
-  render() {
-    const { id } = this.props.match.params;
-    const columns = [
-      { Header:'id',accessor:'__id',show:false},
-      { Header: 'اسم الطالب',accessor:'name'},
-      { Header:'العمر',accessor:'age'},
-      { Header: 'الجنس',accessor:'gender'},
-    ];
-    const fields = [
-      {
-        label: 'أضف طالباً للنشاط',
-        type: 'text',
-        name: 'userName',
-        placeholder: 'اسسم الطالب'
-      }
-    ];
-    return (
-        <React.Fragment>
-          <Header />
-          <StyledActivityDetailsNav>
-            <ActivityDetails activityId={id} />
-          </StyledActivityDetailsNav>
-          <StyledPage>
-            <MainForm fields={fields} action={this.submitAction} operationName="إنشاء" />
-            <Table columns={columns} data={[]}/>
-          </StyledPage>
-        </React.Fragment>
+/*   onSubmitAddStudent({ userName }) {
+     const userNameWithDate = userName;
+     const choosenUser = this.state.suggestions.filter((user) => {
+
+     });
+   }*/
+
+   renderSuggestions() {
+     return this.state.suggestions.map((suggestion, key) => {
+       return <option value={`${suggestion.name} - ${suggestion.dateOfBirth}`} key={key} />;
+     });
+   }
+
+   render() {
+     const { id } = this.props.match.params;
+     const columns = [
+       {
+         Header: 'id',
+         accessor: '__id',
+         show: false
+       },
+       {
+         Header: 'اسم الطالب',
+         accessor: 'name'
+       },
+       {
+         Header: 'العمر',
+         accessor: 'age'
+       },
+       {
+         Header: 'الجنس',
+         accessor: 'gender'
+       },
+     ];
+
+     return (
+       <React.Fragment>
+         <Header/>
+         <StyledActivityDetailsNav>
+           <ActivityDetails activityId={id}/>
+         </StyledActivityDetailsNav>
+         <StyledPage>
+           <Formik>
+             <form>
+               <Field type="text" name="userName" placeholder="اكتب اسم الطالب"
+                      onChange={this.onChangeAction} list="userNames"/>
+               <Button value="إضافة">اضافة</Button>
+               <datalist id="userNames">
+                 {this.renderSuggestions()}
+               </datalist>
+             </form>
+           </Formik>
+           <Table columns={columns} data={[]}/>
+         </StyledPage>
+       </React.Fragment>
 
      );
-    }
-  }
-
+   }
+ }
  export default ActivityInformation;
