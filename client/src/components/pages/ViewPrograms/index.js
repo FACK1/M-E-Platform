361f01bc4285176013c638cf066b67205db338c9
razantayyem <razanr543@gmail.com>
2019-02-18 10:02:34 +0200

    Put program table using Table component #55

--- client/src/components/pages/ViewPrograms/index.js
+++ client/src/components/pages/ViewPrograms/index.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import {Link} from 'react-router-dom'
 import Header from "../../Header";
 import { StyledPage, StyledTilte } from "./index.style";
+import Table from "../../Table";
+
 
 class ViewPrograms extends Component {
+  state={
+    result:[],
+  };
+
+  componentDidMount() {
+   axios
+     .get("/programs")
+     .then(data => {
+       const result = data.data;
+       this.setState({result:result.data});
+     }).catch(() => {
+       alert("error");
+     });
+ }
+
   render() {
+    const columns=[
+      { Header:'id',accessor:'__id',show:false},
+      { Header: 'اسم البرنامج',accessor:'name'},
+      { Header: 'اسم المؤسسة',accessor:'organization'},
+];
+
     return (
       <React.Fragment>
         <Header />
         <StyledPage>
           <StyledTilte> جميع البرامج </StyledTilte>
           <li><Link to="/CreateProgram">إضافة برنامج جديد</Link></li>
+          <Table columns={columns} data={this.state.result}/>
         </StyledPage>
       </React.Fragment>
     );
