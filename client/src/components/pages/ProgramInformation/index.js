9e766828c98031237f39ba99c6e870078caf8f66
razantayyem <razanr543@gmail.com>
2019-02-20 14:34:56 +0200

    fill data in the Table #77

--- client/src/components/pages/ProgramInformation/index.js
+++ client/src/components/pages/ProgramInformation/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { StyledPage, StyledProgramDetailsNav } from "./index.style";
 import Header from "../../Header";
 import ProgramDetails from "../../ProgramDetails";
@@ -8,13 +9,33 @@ class ProgramInformation extends Component {
   constructor(props) {
     super(props);
   }
+  state = {
+    result: []
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.getActivitesByProgramId(id);
+  }
+
+  getActivitesByProgramId = programId => {
+    axios
+      .get(`/activities/getActivitesByProgramId/${programId}`)
+      .then(response => {
+        const result = response.data.activities;
+        this.setState({ result: result });
+      })
+      .catch(() => {
+        alert("error");
+      });
+  };
 
   render() {
     const { id } = this.props.match.params;
     const columns = [
       { Header: "id", accessor: "id", show: false },
       { Header: "اسم النشاط", accessor: "name" },
-      { Header: "عدد الطلاب", accessor: "numberOfstudents" }
+      { Header: "عدد الطلاب", accessor: "usersCount" }
     ];
     return (
       <React.Fragment>
@@ -23,7 +44,7 @@ class ProgramInformation extends Component {
           <ProgramDetails programId={id} />
         </StyledProgramDetailsNav>
         <StyledPage>
-          <Table columns={columns} data={[]} />
+          <Table columns={columns} data={this.state.result} />
         </StyledPage>
       </React.Fragment>
     );
