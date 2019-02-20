ae4fb1b32887cb132943fb27935a2301af07104d
fdo2 <fdo2@live.com>
2019-02-20 10:02:43 +0200

    creat a link for creating activity in programme page #19

--- client/src/components/pages/ProgramInformation/index.js
+++ client/src/components/pages/ProgramInformation/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { StyledPage, StyledProgramDetailsNav } from "./index.style";
+import {Link} from 'react-router-dom'
 import Header from "../../Header";
 import ProgramDetails from "../../ProgramDetails";
 import Table from "../../Table";
@@ -23,7 +24,8 @@ class ProgramInformation extends Component {
           <ProgramDetails programId={id} />
         </StyledProgramDetailsNav>
         <StyledPage>
-          <Table columns={columns} data={[]} />
+        <li><Link to="/CreateActivity">إضافة نشاط جديد</Link></li>
+        <Table columns={columns} data={[]} />
         </StyledPage>
       </React.Fragment>
     );
