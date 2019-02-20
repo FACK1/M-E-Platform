dfdb0ec9e6b262bbd3248bf4730ea0519b780368
razantayyem <razanr543@gmail.com>
2019-02-20 14:22:36 +0200

    Create router for getActivitesByProgramId #77

--- src/router.js
+++ src/router.js
@@ -9,10 +9,12 @@ router.post('/activities', controllers.activity.add);
 router.get('/activities', controllers.activity.findAll);
 router.get('/activities/:id', controllers.activity.findById);
 router.post('/activities/addUserToActivity', controllers.activity.addUserToActivity);
+router.get('/activities/getActivitesByProgramId/:programId', controllers.activity.getActivitesByProgramId);
 
 router.post('/programs', controllers.program.add);
 router.get('/programs', controllers.program.findAll);
 router.get('/programs/:id', controllers.program.findById);
+
 
 router.post('/users', controllers.user.add);
 router.get('/users', controllers.user.findAll);
