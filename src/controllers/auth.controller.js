7fa8c3c1ab4b280c448d5519f4548b775e4d9f79
mhmdtshref <mhmdtshref@gmail.com>
2019-02-21 11:53:11 +0200

    Use bcrypt and User model to register a new admin #85

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,26 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
+const saveUser = (user) => new Promise((resolve, reject) => {
+  user.save((err, user) => {
+    if(err){
+      reject(err);
+    } else {
+      resolve(user);
+    }
+  });
+});
 
 const register = (req, res) => {
   const { name, living, address, gender, dateOfBirth, phoneNo, email, password } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => new User({ name, living, address, gender, dateOfBirth, phoneNo, email, password: hash, type: 'admin' }))
+    .then(saveUser)
+    .catch((err) => {
+      res.json({ success: false, err: err.message });
+    });
+};
+
+module.exports = {
+  register,
 };
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,11 +2,12 @@ const activity = require('./activity.controller');
 const program = require('./program.controller');
 const user = require('./user.controller');
 const objective = require('./objective.controller');
-
+const auth = require('./auth.controller');
 
 module.exports = {
   activity,
   program,
   user,
   objective,
+  auth,
 };
