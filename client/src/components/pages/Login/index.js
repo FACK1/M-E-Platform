7a0ef211abb462c61d704881c864f99931022082
razantayyem <razanr543@gmail.com>
2019-02-21 14:10:21 +0200

    Update style #83

--- client/src/components/pages/Login/index.js
+++ client/src/components/pages/Login/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import logo from '../../../assets/Logo.png';
 import MainForm from "../../Form";
-import { Title, StyledPage } from "./index.style";
+import bgLogo from '../../../assets/background-YDRC-Logo.png';
+
+import { Title, StyledPage, StyledDiv, StyledLogo, StyledBgLogo, Styleddiv, Styleddivv} from "./index.style";
+
 
 class Login extends Component {
   render() {
@@ -20,13 +24,21 @@ class Login extends Component {
     ];
     return (
       <React.Fragment>
-        <StyledPage>
+      <StyledDiv>
+      <StyledLogo src={logo} />
+      <StyledBgLogo src={bgLogo}/>
+      </StyledDiv>
+      <StyledPage>
+        <Styleddiv>
           <Title>تسجيل الدخول الى حسابك </Title>
+          <Styleddivv>
           <MainForm
             fields={fields}
             action={this.submitAction}
             operationName="تسجيل الدخول الى حسابك"
           />
+          </Styleddivv>
+          </Styleddiv>
         </StyledPage>
       </React.Fragment>
     );
--- client/src/components/pages/Login/index.style.js
+++ client/src/components/pages/Login/index.style.js
@@ -1,20 +1,56 @@
 import styled from "styled-components";
 import { Form, Field } from "formik";
 
-export const Title = styled.h1`
-  font-weight: 600;
-  color: #4d4d4d;
-  font-size: 2.2em;
-`;
 
 export const StyledPage = styled.div`
-  margin-right: 25%;
+  margin-right: 40%;
   padding-top: 15vh;
+`;
+
+export const Styleddiv = styled.div`
+width: 100%;
+height:70px;
+left: 87px;
+top: 79px;
+border: 1px solid;
+color:white;
+padding: 10px;
+`;
+
+export const StyledDiv = styled.div`
+display: flex;
+flex-direction:column;
+width:40%;
+position:fixed;
+float:right;
+background-image: linear-gradient(-180deg, rgba(25,179,212,1), rgba(255,255,255,0));
+background-color:  #4BC0D8;
+height:100vh;
+padding: 0;
+`;
+export const Styleddivv = styled.div`
+display: flex;
+flex-direction:column;
+`;
+
+export const StyledLogo= styled.img`
+width:60%;
+align-self:center;
+border-radius: 70%;
+width:40%;
+margin-top:50%;
+`;
+
+export const StyledBgLogo= styled.img`
+position:absolute;
+bottom:0;
+right:0;
+width:100%;
+margin-right:100px;
 `;
 
 export const StyledForm = styled(Form)`
   display: flex;
-  justify-content: space-between;
   flex-direction: columns;
   align-items: center;
   flex-wrap: wrap;
@@ -22,6 +58,11 @@ export const StyledForm = styled(Form)`
   margin-top: 80px;
   margin-left: 50px;
   margin-buttom: 100px;
+`;
+export const Title = styled.h1`
+  font-weight: 600;
+  color: #19B3D4;
+  font-size: 2.2em;
 `;
 
 export const StyledLabel = styled.label`
