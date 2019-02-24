7c9f62af28e47462707f0b5256738225801ddf52
mhmdtshref <mhmdtshref@gmail.com>
2019-02-24 11:18:42 +0200

    Update model to make fields together unique #93

--- src/models/ActivitiesUser.js
+++ src/models/ActivitiesUser.js
@@ -6,5 +6,6 @@ const ActivitiesUserSchema = mongoose.Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   activity: { type: Schema.Types.ObjectId, ref: 'Activity' },
 });
+ActivitiesUserSchema.index({ user: 1, activity: 1 }, { unique: true });
 
 module.exports = mongoose.model('ActivitiesUser', ActivitiesUserSchema);
